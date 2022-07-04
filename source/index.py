1e04eac0cf2ccd76aec88deb8ea483226dbd6b47
robertoDorado <robertodorado7@gmail.com>
2022-07-04 18:20:59 -0300

    versão inicial do bot feita

--- source/index.py
+++ source/index.py
@@ -1,12 +1,38 @@
 #_*_ coding: utf-8 _*_
-from iqoption.connection import Connection_API_IQ_Option
+from iqoption.connection import BOT_IQ_Option
 
-API = Connection_API_IQ_Option()
+API = BOT_IQ_Option()
 
 if API.check_my_connection() == False:
     print('erro na conexão')
     exit()
 
-instance = API.get_instance()
-print(API.convert_timestamp())
-# print(instance.get_digital_payout('EURUSD'))
+value = 2.75
+
+while True:
+    try:
+        
+        ticks = API.get_ticks_real_time('EURUSD-OTC')
+        
+        for tick in ticks:
+            
+            pointer = round(ticks[tick]['close'], 4)
+            print(pointer)
+        
+            if pointer == 0.8989:
+                print(f'venda realizada de: {value}')
+                status, id = API.call_or_put(value, 'EURUSD-OTC', 'put', 1)
+                
+                if status:
+                    print(API.check_win_or_loss(id, 'v4'))
+            
+            if pointer == 0.8982:
+                print(f'compra realizada de: {value}')         
+                status, id = API.call_or_put(value, 'EURUSD-OTC', 'call', 1)
+                
+                if status:
+                    print(API.check_win_or_loss(id, 'v4'))
+              
+        API.set_time_sleep(1)
+    except Exception as error:
+        print(f'something wrong: {error}')
--- source/iqoption/connection.py
+++ source/iqoption/connection.py
@@ -1,10 +1,9 @@
-from typing_extensions import Self
 from iqoptionapi.stable_api import IQ_Option
 import time
 from datetime import datetime
 from pytz import timezone
 
-class Connection_API_IQ_Option:
+class BOT_IQ_Option:
     
     def __init__(self):
         self.email = 'robertodorado7@gmail.com'
@@ -29,20 +28,57 @@ class Connection_API_IQ_Option:
         profile = self.instance.get_profile_ansyc()
         return profile[key]
     
-    def get_all_candles(self, active='EURUSD', quant=3):
+    def get_all_candles(self, active='EURUSD', seconds=60, quant=3):
         
-        while True:
-            try:
-                dictionary_actives = self.instance.get_all_ACTIVES_OPCODE()
-                for actives in list(dictionary_actives.keys()):
-                    if actives == active:
-                        return self.instance.get_candles(active, 60, quant, self.current_timestamp)
-            except Exception:
-                print('is not a valid active')
-                break
+        try:
+            dictionary_actives = self.instance.get_all_ACTIVES_OPCODE()
+            for actives in list(dictionary_actives.keys()):
+                if actives == active:
+                    return self.instance.get_candles(active, seconds, quant, self.current_timestamp)
+        except Exception:
+            print('is not a valid active')
+        
             
     def get_current_timestamp(self):
         return self.current_timestamp
     
     def convert_timestamp(self, timestamp=time.time(), tz='America/Sao_Paulo'):
         return datetime.fromtimestamp(timestamp, timezone(tz))
+    
+    def merge_candles(self, candle_a, candle_b):
+        return candle_a + candle_b
+    
+    def set_time_sleep(self, value):
+        return time.sleep(value)
+    
+    def get_ticks_real_time(self, active='EURUSD', size=60, maxdict=1):
+        
+        self.instance.start_candles_stream(active, size, maxdict)
+        ticks = self.instance.get_realtime_candles(active, size)
+        
+        self.instance.stop_candles_stream(active, size)
+        return ticks
+    
+    def get_mood_traders(self, active='EURUSD'):
+        
+        self.instance.start_mood_stream(active)
+        mood = self.instance.get_traders_mood(active)
+        
+        self.instance.stop_mood_stream(active)
+        return mood
+    
+    def get_all_actives(self):
+        return {code:act for act,code in self.instance.get_all_ACTIVES_OPCODE().items()}
+    
+    def call_or_put(self, price, active, action, timeframe):
+        return self.instance.buy(price, active, action, timeframe)
+    
+    def check_win_or_loss(self, id, version):
+        
+        try:
+            if version == 'v3':
+                return self.instance.check_win_v3(id)
+            if version == 'v4':
+                return self.instance.check_win_v4(id)
+        except Exception:
+            print('version or id is not available')
