409e0faa7d4ee23d06245f9bcfa8a600b82cb006
robertoDorado <robertodorado7@gmail.com>
2022-07-06 18:22:59 -0300

    teste na otimização de acertos utilizando as cores das velas

--- source/index.py
+++ source/index.py
@@ -20,18 +20,22 @@ while True:
     try:
         
         candles = API.get_all_candles('EURUSD-OTC', 60, 100)
-        candles = [i['close'] for i in candles]
         
         ticks = API.get_ticks_real_time('EURUSD-OTC')
+        
+        candles = [{'close': i['close'], 'open': i['open'], 
+        'candle_color': 'red_candle' if i['open'] > i['close'] 
+        else 'green_candle' if i['open'] < i['close'] else 'dogi'} for i in candles]
+        
+        red_candles = [x['candle_color'] for x in candles if x['candle_color'] == 'red_candle']
+        green_candles = [x['candle_color'] for x in candles if x['candle_color'] == 'green_candle']
         
         for tick in ticks:
             
             pointer = round(ticks[tick]['close'], 4)
-            max_pointer = round(max(candles), 4)
-            min_pointer = round(min(candles), 4)
             print(pointer)
         
-            if pointer == max_pointer:
+            if len(green_candles) > len(red_candles):
                 
                 print(f'venda realizada de: R$ {value}')
                 status, id = API.call_or_put(value, 'EURUSD-OTC', 'put', 1)
@@ -44,13 +48,15 @@ while True:
                         win_value.append(check_value)
                         
                         print(f'total wins: {len(wins)}, R$ {round(sum(win_value), 2)}')
+                        API.set_time_sleep(60)
                     else:
                         loss.append(status)
                         loss_value.append(check_value)
                         
                         print(f'total loss: {len(loss)}, - R$ {round(sum(loss_value), 2) * - 1}')
+                        API.set_time_sleep(60)
             
-            if pointer == min_pointer:
+            if len(green_candles) < len(red_candles):
                 
                 print(f'compra realizada de: R$ {value}')
                 status, id = API.call_or_put(value, 'EURUSD-OTC', 'call', 1)
@@ -63,11 +69,13 @@ while True:
                         win_value.append(check_value)
                         
                         print(f'total wins: {len(wins)}, R$ {round(sum(win_value), 2)}')
+                        API.set_time_sleep(60)
                     else:
                         loss.append(status)
                         loss_value.append(check_value)
                         
                         print(f'total loss: {len(loss)}, -R$ {round(sum(loss_value), 2) * - 1}')
+                        API.set_time_sleep(60)
                             
         API.set_time_sleep(1)
     except Exception as error:
