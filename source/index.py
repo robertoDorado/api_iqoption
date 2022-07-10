f3b4fe8f205cf2764fdba6b8d0f2ca5b28381561
robertoDorado <robertodorado7@gmail.com>
2022-07-09 21:06:22 -0300

    ajuste para aumentar a expectativa de acertos

--- source/index.py
+++ source/index.py
@@ -16,12 +16,15 @@ actives = API.get_all_actives()
 actives_code = [1, 3, 5]
 actives_code_otc = [76, 77, 79]
 
-if date.hour > 14:
+if date.weekday() == 5 or date.weekday() == 6:
     actives_code = choices(actives_code_otc)[0]
+    print(f'iniciando a busca no ativo {actives[actives_code]}')
 else:
     actives_code = choices(actives_code)[0]
+    print(f'iniciando a busca no ativo {actives[actives_code]}')
 
 profit = API.get_profit(actives[actives_code], 'turbo') * 100
+balance = API.balance()
     
 value = 1000
 
@@ -30,6 +33,9 @@ loss = []
 
 win_value = []
 loss_value = []
+
+red_analitics = []
+green_analitics = []
 i = 0
 
 while True:
@@ -38,16 +44,18 @@ while True:
         
         i = i + 1
         
-        if i == 10:
-            if date.hour > 14:
+        if i == 10 or i == 20 or i == 30:
+            if date.weekday() == 5 or date.weekday() == 6:
                 actives_code = choices(actives_code_otc)[0]
                 print(f'buscando em {actives[actives_code]}')
             else:
                 actives_code = choices(actives_code)[0]
                 print(f'buscando em {actives[actives_code]}')
-            i = 0
+        elif i > 30:
+            print('mercado ruim para efetuar operações de trade')
+            exit()
         
-        candles = API.get_all_candles(actives[actives_code], 60, 11)
+        candles = API.get_all_candles(actives[actives_code], 60, 12)
 
         candles = [{'close': i['close'], 'open': i['open'],
         'candle_color': 'red_candle' if i['open'] > i['close']
@@ -61,95 +69,72 @@ while True:
         total_candles = len(green_candles) + len(red_candles)
         percent_green = round(len(green_candles) / total_candles * 100)
         percent_red = round(len(red_candles) / total_candles * 100)
-
-        if percent_green >= 75 and percent_green > percent_red:
-            
-            if value > 0:
-                status, id = API.call_or_put(value, actives[actives_code], 'put', 1)
-            else:
-                print('saldo insuficiente')
-                exit()
-                
-            print(f'venda realizada de: R$ {round(value, 2)}')
-            
-            if status:
-                status, check_value = API.check_win_or_loss(id, 'v4')
-                
-                if status == 'win':
-                    wins.append(status)
-                    win_value.append(check_value)
-                    
-                    if len(wins) > 0 or len(loss) > 0:
-                        
-                        total_universe = len(wins) + len(loss)
-                    
-                        prob_wins = len(wins) / total_universe
-                        prob_loss = 1 - prob_wins
-                    
-                        k = API.kelly_discretion(prob_wins, prob_loss, profit)
-                        value = value + (value * (round(k, 2) / 100))
-                    
-                    print(f'total wins: {len(wins)}, R$ {round(sum(win_value), 2)}')
-                else:
-                    loss.append(status)
-                    loss_value.append(check_value)
-                    
-                    if len(wins) > 0 or len(loss) > 0:
-                        
-                        total_universe = len(wins) + len(loss)
-                
-                        prob_wins = len(wins) / total_universe
-                        prob_loss = 1 - prob_wins
-                    
-                        k = API.kelly_discretion(prob_wins, prob_loss, profit)
-                        value = value - (value * (round(k, 2) / 100))
-                    
-                    print(f'total loss: {len(loss)}, - R$ {round(sum(loss_value), 2) * - 1}')
         
-        if percent_red >= 75 and percent_red > percent_green:
+        if len(red_analitics) <= 3 and len(green_analitics) <= 3:
             
-            if value > 0:
-                status, id = API.call_or_put(value, actives[actives_code], 'call', 1)
-            else:
-                print('saldo insuficiente')
-                exit()
-                
-            print(f'compra realizada de: R$ {round(value, 2)}')
+            red_analitics.append(percent_red)
+            green_analitics.append(percent_green)
             
-            if status:
-                status, check_value = API.check_win_or_loss(id, 'v4')
+            if sum(green_analitics) > sum(red_analitics):
+                critical_percent = sum(green_analitics) - sum(red_analitics)
+            elif sum(green_analitics) < sum(red_analitics):
+                critical_percent = sum(red_analitics) - sum(green_analitics)
                 
-                if status == 'win':
-                    wins.append(status)
-                    win_value.append(check_value)
-                    
-                    if len(wins) > 0 or len(loss) > 0:
-                        
-                        total_universe = len(wins) + len(loss)
+            if green_analitics > red_analitics and critical_percent > 100:
                 
-                        prob_wins = len(wins) / total_universe
-                        prob_loss = 1 - prob_wins
-                        
-                        k = API.kelly_discretion(prob_wins, prob_loss, profit)
-                        value = value + (value * (round(k, 2) / 100))
-                    
-                    print(f'total wins: {len(wins)}, R$ {round(sum(win_value), 2)}')
+                print(f'venda: {sum(green_analitics)}%')
+                print(f'compra: {sum(red_analitics)}%')
+                
+                if balance > value:
+                    status, id = API.call_or_put(value, actives[actives_code], 'put', 1)
                 else:
-                    loss.append(status)
-                    loss_value.append(check_value)
+                    print('saldo insuficiente')
+                    exit()
                     
-                    if len(wins) > 0 or len(loss) > 0:
-                        
-                        total_universe = len(wins) + len(loss)
+                print(f'venda realizada de: R$ {round(value, 2)}')
                 
-                        prob_wins = len(wins) / total_universe
-                        prob_loss = 1 - prob_wins
-                        
-                        k = API.kelly_discretion(prob_wins, prob_loss, profit)
-                        value = value - (value * (round(k, 2) / 100))
-                        
+                if status:
+                    status, check_value = API.check_win_or_loss(id, 'v4')
                     
-                    print(f'total loss: {len(loss)}, -R$ {round(sum(loss_value), 2) * - 1}')
+                    if status == 'win':
+                        wins.append(status)
+                        win_value.append(check_value)
+                        value = value + (value * 0.10)
+                        
+                        print(f'total wins: {len(wins)}, R$ {round(sum(win_value), 2)}')
+                    else:
+                        loss.append(status)
+                        loss_value.append(check_value)
+                        value = value + (value * 0.10)
+                        
+                        print(f'total loss: {len(loss)}, - R$ {round(sum(loss_value), 2) * - 1}')
+            
+            if red_analitics > green_analitics and critical_percent > 100:
+                
+                print(f'venda: {sum(green_analitics)}%')
+                print(f'compra: {sum(red_analitics)}%')
+                
+                if balance > value:
+                    status, id = API.call_or_put(value, actives[actives_code], 'call', 1)
+                else:
+                    print('saldo insuficiente')
+                    exit()
+                    
+                print(f'compra realizada de: R$ {round(value, 2)}')
+                
+                if status:
+                    status, check_value = API.check_win_or_loss(id, 'v4')
+                    
+                    if status == 'win':
+                        wins.append(status)
+                        win_value.append(check_value)
+                        
+                        print(f'total wins: {len(wins)}, R$ {round(sum(win_value), 2)}')
+                    else:
+                        loss.append(status)
+                        loss_value.append(check_value)
+                        
+                        print(f'total loss: {len(loss)}, -R$ {round(sum(loss_value), 2) * - 1}')
                             
         API.set_time_sleep(1)
     except Exception as error:
