6cdf894314a89e69e48c8b2dee4317d11e389bbb
robertoDorado <robertodorado7@gmail.com>
2022-07-26 16:31:37 -0300

    mapeando velas de 5 e 15 minutos

--- source/index.py
+++ source/index.py
@@ -17,6 +17,18 @@ print(f'my account is {account_type}: R$ {balance}')
 while True:
     
     try:
-        name = ''
+        five_minutes_candle = API.get_all_candles('EURUSD', 300, 1)
+        fifteen_minutes_candle = API.get_all_candles('EURUSD', 900, 1)
+        
+        candle_fifteen_m = [{'candle': 'red' if i['open'] > i['close']
+        else 'green' if i['open'] < i['close'] else 'dogi', 'open': i['open'], 'close': i['close']}
+        for i in fifteen_minutes_candle]
+        
+        candle_five_m = [{'candle': 'red' if i['open'] > i['close']
+        else 'green' if i['open'] < i['close'] else 'dogi', 'open': i['open'], 'close': i['close']}
+        for i in five_minutes_candle]
+        
+        print(candle_fifteen_m)
+        API.set_time_sleep(1)
     except Exception as error:
         print(f'something wrong: {error}')
