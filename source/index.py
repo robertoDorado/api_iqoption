42e01b8f8dc6e481b9c99cb55ccbeee396035781
robertoDorado <robertodorado7@gmail.com>
2022-08-03 15:19:41 -0300

    incluindo estrategias de pivot

--- source/index.py
+++ source/index.py
@@ -16,7 +16,7 @@ wins = []
 stop_loss = []
 
 value = 10000
-active_index = 1
+active_index = 2
 active = API.get_all_actives()[active_index]
 
 total_candles = 80
@@ -37,9 +37,30 @@ while True:
     
     try:
         
-        seconds += 1 
+        seconds += 1
+        
+        indicators = instance.get_technical_indicators(active)
+        
+        if 'code' in indicators:
+        
+            if indicators['code'] == 'no_technical_indicator_available':
+                
+                seconds = 0
+                active_index += 1
+                
+                if otc and active_index > 80:
+                    active_index = 76
+                    
+                if mkt and active_index > 4:
+                    active_index = 1
+                    
+                active = API.get_all_actives()[active_index]
+                indicators = instance.get_technical_indicators(active)
+                historic_five_minutes = API.get_realtime_candles(active, 300, total_candles)
+                print(f'mudando para o ativo {active}')
         
         if seconds == 300:
+            
             seconds = 0
             active_index += 1
             
@@ -50,26 +71,33 @@ while True:
                 active_index = 1
                 
             active = API.get_all_actives()[active_index]
+            indicators = instance.get_technical_indicators(active)
+            historic_five_minutes = API.get_realtime_candles(active, 300, total_candles)
             print(f'mudando para o ativo {active}')
         
+        indicators = [i for i in indicators if i['group'] == 'PIVOTS']
+        indicators = [i for i in indicators if i['candle_size'] == 300]
+        
+        s3 = [i['value'] for i in indicators if i['name'] == 'Classic s3']
+        s2 = [i['value'] for i in indicators if i['name'] == 'Classic s2']
+        s1 = [i['value'] for i in indicators if i['name'] == 'Classic s1']
+        
+        p = [i['value'] for i in indicators if i['name'] == 'Classic p']
+        
+        r1 = [i['value'] for i in indicators if i['name'] == 'Classic r1']
+        r2 = [i['value'] for i in indicators if i['name'] == 'Classic r2']
+        r3 = [i['value'] for i in indicators if i['name'] == 'Classic r3']
+        
         historic_five_minutes = API.get_realtime_candles(active, 300, total_candles)
-        historic_fifteen_minutes = API.get_all_candles(active, 900, 1)
         
         historic_five_minutes = [{'candle': 'red' if historic_five_minutes[i]['open'] > historic_five_minutes[i]['close']
         else 'green' if historic_five_minutes[i]['close'] > historic_five_minutes[i]['open'] else 'dogi',
-        'max': historic_five_minutes[i]['max'] if 'max' in historic_five_minutes[i] else NULL,
-        'min': historic_five_minutes[i]['min'] if 'min' in historic_five_minutes[i] else NULL,
         'close': historic_five_minutes[i]['close']}
         for i in historic_five_minutes]
         
-        
-        all_candles_five_m = [i['candle'] for i in historic_five_minutes]
-        all_candle_max_five_m = [i['max'] for i in historic_five_minutes]
-        all_candle_min_five_m = [i['min'] for i in historic_five_minutes]
         all_candle_close_five_m = [i['close'] for i in historic_five_minutes]
         
-        max_value_reference = max(all_candle_max_five_m)
-        min_value_reference = min(all_candle_min_five_m)
+        first_candle_index = len(all_candle_close_five_m) - 1
         
         red = [i['candle'] for i in historic_five_minutes if i['candle'] == 'red']
         green = [i['candle'] for i in historic_five_minutes if i['candle'] == 'green']
@@ -106,50 +134,7 @@ while True:
             print('tendencia consolidada')
             
         
-        first_max_candle_index = len(all_candle_max_five_m) - 1
-        first_min_candle_index = len(all_candle_min_five_m) - 1
-        first_close_candle_index = len(all_candle_close_five_m) - 1
-        
-        if low_tendencie == True and all_candle_close_five_m[first_close_candle_index] < min_value_reference:
-                
-                if balance >= value:
-                    status, id = API.call_or_put(value, active, 'put', 1)
-                else:
-                    print('saldo insuficiente')
-                    exit()
-                
-                print(f'venda: {value}')
-                
-                if status:
-                    status, check_value = API.check_win_or_loss(id, 'v4')
-                    
-                    if status == 'win':
-                        wins.append(status)
-                        print(f'total wins: {len(wins)}')
-                        
-                        active_index += 1
-                        
-                        if otc and active_index > 80:
-                            active_index = 76
-                            
-                        if mkt and active_index > 4:
-                            active_index = 1
-                            
-                        print(f'mudando para o ativo {active}')
-                        
-                        # if len(wins) == 2:
-                        #     print('meta batida')
-                        #     exit()
-                    else:
-                        stop_loss.append(status)
-                        print(f'total loss: {len(stop_loss)}')
-                        
-                        if len(stop_loss) == 1:
-                            print('stop loss acionado')
-                            exit()
-        
-        
-        if height_tendencie == True and all_candle_close_five_m[first_close_candle_index] > max_value_reference:
+        if all_candle_close_five_m[first_candle_index] == s1[0] or all_candle_close_five_m[first_candle_index] == s2[0] or all_candle_close_five_m[first_candle_index] == s3[0]:
                 
                 if balance >= value:
                     status, id = API.call_or_put(value, active, 'call', 1)
@@ -174,6 +159,134 @@ while True:
                         if mkt and active_index > 4:
                             active_index = 1
                             
+                        active = API.get_all_actives()[active_index]
+                        indicators = instance.get_technical_indicators(active)
+                        historic_five_minutes = API.get_realtime_candles(active, 300, total_candles)
+                        print(f'mudando para o ativo {active}')
+                        
+                        # if len(wins) == 2:
+                        #     print('meta batida')
+                        #     exit()
+                    else:
+                        stop_loss.append(status)
+                        print(f'total loss: {len(stop_loss)}')
+                        
+                        if len(stop_loss) == 1:
+                            print('stop loss acionado')
+                            exit()
+                            
+        if all_candle_close_five_m[first_candle_index] == r1[0] or all_candle_close_five_m[first_candle_index] == r2[0] or all_candle_close_five_m[first_candle_index] == r3[0]:
+                
+                if balance >= value:
+                    status, id = API.call_or_put(value, active, 'put', 1)
+                else:
+                    print('saldo insuficiente')
+                    exit()
+                
+                print(f'venda: {value}')
+                
+                if status:
+                    status, check_value = API.check_win_or_loss(id, 'v4')
+                    
+                    if status == 'win':
+                        wins.append(status)
+                        print(f'total wins: {len(wins)}')
+                        
+                        active_index += 1
+                        
+                        if otc and active_index > 80:
+                            active_index = 76
+                            
+                        if mkt and active_index > 4:
+                            active_index = 1
+                            
+                        active = API.get_all_actives()[active_index]
+                        indicators = instance.get_technical_indicators(active)
+                        historic_five_minutes = API.get_realtime_candles(active, 300, total_candles)
+                        print(f'mudando para o ativo {active}')
+                        
+                        # if len(wins) == 2:
+                        #     print('meta batida')
+                        #     exit()
+                    else:
+                        stop_loss.append(status)
+                        print(f'total loss: {len(stop_loss)}')
+                        
+                        if len(stop_loss) == 1:
+                            print('stop loss acionado')
+                            exit()
+
+        
+        if low_tendencie == True and all_candle_close_five_m[first_candle_index] < s1[0] or all_candle_close_five_m[first_candle_index] < s2[0] or all_candle_close_five_m[first_candle_index] < s3[0]:
+                
+                if balance >= value:
+                    status, id = API.call_or_put(value, active, 'put', 1)
+                else:
+                    print('saldo insuficiente')
+                    exit()
+                
+                print(f'venda: {value}')
+                
+                if status:
+                    status, check_value = API.check_win_or_loss(id, 'v4')
+                    
+                    if status == 'win':
+                        wins.append(status)
+                        print(f'total wins: {len(wins)}')
+                        
+                        active_index += 1
+                        
+                        if otc and active_index > 80:
+                            active_index = 76
+                            
+                        if mkt and active_index > 4:
+                            active_index = 1
+                            
+                        active = API.get_all_actives()[active_index]
+                        indicators = instance.get_technical_indicators(active)
+                        historic_five_minutes = API.get_realtime_candles(active, 300, total_candles)
+                        print(f'mudando para o ativo {active}')
+                        
+                        # if len(wins) == 2:
+                        #     print('meta batida')
+                        #     exit()
+                    else:
+                        stop_loss.append(status)
+                        print(f'total loss: {len(stop_loss)}')
+                        
+                        if len(stop_loss) == 1:
+                            print('stop loss acionado')
+                            exit()
+        
+        
+        if height_tendencie == True and all_candle_close_five_m[first_candle_index] > r1[0] or all_candle_close_five_m[first_candle_index] > r2[0] or all_candle_close_five_m[first_candle_index] > r3[0]:
+                
+                if balance >= value:
+                    status, id = API.call_or_put(value, active, 'call', 1)
+                else:
+                    print('saldo insuficiente')
+                    exit()
+                
+                print(f'compra: {value}')
+                
+                if status:
+                    status, check_value = API.check_win_or_loss(id, 'v4')
+                    
+                    if status == 'win':
+                        wins.append(status)
+                        print(f'total wins: {len(wins)}')
+                        
+                        active_index += 1
+                        
+                        if otc and active_index > 80:
+                            active_index = 76
+                            
+                        if mkt and active_index > 4:
+                            active_index = 1
+                            
+                        active = API.get_all_actives()[active_index]
+                        indicators = instance.get_technical_indicators(active)
+                        historic_five_minutes = API.get_realtime_candles(active, 300, total_candles)
                         print(f'mudando para o ativo {active}')
                         
                         # if len(wins) == 2:
